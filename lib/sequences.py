5042bd85b502e4de559e837a9000ad1d7853d998
joel <joel.nganga@student.moringaschool.com>
2025-05-17 18:28:37 +0300

    Add sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
 
 def print_fibonacci(length):
     if length <= 0:
         print("[]")
+    elif length == 1:
+        print("[0]")
+    elif length == 2:
+        print("[0, 1]")
     else:
         fib_sequence = [0, 1]
-        for i in range(2, length):
+        while len(fib_sequence) < length:
             fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-        print(fib_sequence[:length])
+        print(fib_sequence)
